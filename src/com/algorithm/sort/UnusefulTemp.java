0a5e221955d5a51c64be007458e81f4bfc36b602
Egnaxela <yougensai@gmail.com>
2018-04-11 11:58:35 +0800

    :zap:

--- src/com/algorithm/sort/UnusefulTemp.java
+++ src/com/algorithm/sort/UnusefulTemp.java
@@ -34,12 +34,16 @@ public class UnusefulTemp {
 		int[] arr=SortHelper.generateRandom(10, 0, 100);
 		int[] arr2=Arrays.copyOf(arr, arr.length);
 		int[] arr3=Arrays.copyOf(arr, arr.length);
+		int[] arr4=Arrays.copyOf(arr, arr.length);
+		System.out.println(Arrays.toString(arr2));
 		selectionSort(arr);
 		insertSort(arr2);
 		insertSort2(arr3);
+		quickSort(arr4);
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(arr2));
 		System.out.println(Arrays.toString(arr3));
+		System.out.println(Arrays.toString(arr4));
 	}
 	
 	public  static void  selectionSort(int[] arr) {
@@ -70,6 +74,63 @@ public class UnusefulTemp {
 			arr[j]=temp;
 		}
 	}
+	
+	
+	public static void quickSort(int[] arr) {
+		sort(arr,0,arr.length-1);
+	}
+	
+	private static void sort(int[] arr,int l,int r) {
+		if(l>=r) return;
+		int p=partition(arr, l, r);
+		sort(arr, l, p-1);
+		sort(arr, p+1, r);
+		
+	}
+	
+	/**
+	 * 
+	* <p>描述:使得arr[l ... p-1] <arr[p],
+	* arr[p+1 ... r] >arr[p]
+	* 
+	*  </p>
+	* @param arr
+	* @param l
+	* @param r
+	* @return
+	* @author Eg
+	 */
+	private static int partition(int[] arr,int l,int r) {
+		int v=arr[l];
+		int j=l;
+		for(int i=l+1;i<=r;i++) {
+			if(arr[i]<v) {
+				SortHelper.swap(arr, j+1, i);
+				j++;
+			}
+		}
+		SortHelper.swap(arr, l, j);
+		return j;
+	}
+	
+	/**
+	 * 
+	 */
+	private static int partition2(int[] arr,int l,int r) {
+		int v=arr[l];
+		int j=l;
+		for(int i=l+1;i<=r;i++) {
+			if(arr[i]<v) {
+				j++;
+				SortHelper.swap(arr, j, i);
+			}
+		}
+		SortHelper.swap(arr, l, j);
+		return j;
+		
+	}
+	
+	
 	
 	
 	

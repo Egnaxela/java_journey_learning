5486fcd951e832801443b9f0a173ccd8127c9adf
Egnaxela <yougensai@gmail.com>
2019-03-06 15:54:56 +0800

    :beers:

---
+++ src/com/thinkinginjava/chapter3/E01_HelloDate.java
@@ -0,0 +1,39 @@
+/**  
+ * FileName:     
+ * @Description: 
+ * Company       
+ * @version      1.0
+ * @author:      Pluto
+ * @version:     1.0
+ * Createdate:   2019年3月6日 下午3:28:14  
+ *  
+ */  
+
+package com.thinkinginjava.chapter3;
+
+import static net.mindview.util.Print.print;
+
+import java.util.Date;;
+
+/**  
+ * Description:   
+ * Copyright:   Copyright (c)2019 
+ * Company:       
+ * @author:     Pluto 
+ * @version:    1.0  
+ * Create at:   2019年3月6日 下午3:28:14  
+ *  
+ * Modification History:  
+ * Date         Author      Version     Description  
+ * ------------------------------------------------------------------  
+ * 2019年3月6日      Pluto       1.0         1.0 Version  
+ */
+
+public class E01_HelloDate {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, it's: " +new Date());
+		print("Hello, it's: " +new Date());
+	}
+	
+}

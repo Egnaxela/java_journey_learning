fe7660a43543ea0e337ca484a56c0a1f5435f831
Egnaxela <yougensai@gmail.com>
2019-03-14 16:55:02 +0800

    :beers:

--- src/com/highconcurrencypractice/ThreadTest.java
+++ src/com/highconcurrencypractice/ThreadTest.java
@@ -1,41 +1,41 @@
 /**  
  * FileName:     
  * @Description: 
- * Company       rongji
+ * Company       
  * @version      1.0
- * @author:      Eg 
+ * @author:      Pluto
  * @version:     1.0
- * Createdate:   2018年3月19日 下午3:52:59  
+ * Createdate:   2019年3月14日 下午4:41:19  
  *  
  */  
 
-package com.practicehighconcurrency;
+package com.highconcurrencypractice;
 
 /**  
  * Description:   
- * Copyright:   Copyright (c)2017 
- * Company:     
- * @author:     Eg  
+ * Copyright:   Copyright (c)2019 
+ * Company:       
+ * @author:     Pluto 
  * @version:    1.0  
- * Create at:   2018年3月19日 下午3:52:59  
+ * Create at:   2019年3月14日 下午4:41:19  
  *  
  * Modification History:  
  * Date         Author      Version     Description  
  * ------------------------------------------------------------------  
- * 2018年3月19日      Eg          1.0         1.0 Version  
+ * 2019年3月14日      Pluto       1.0         1.0 Version  
  */
 
 public class ThreadTest {
 	
-	
 	private int count=0;
+	
 	private void add() {
 		for(int i=0;i<10000;i++) {
 			count++;
 		}
 	}
 	
-	public static int calc() throws InterruptedException {
+	private static int calc() throws InterruptedException {
 		ThreadTest test=new ThreadTest();
 		Thread t1=new Thread() {
 			@Override
@@ -51,23 +51,18 @@ public class ThreadTest {
 			}
 		};
 		
-		
-		//启动线程
 		t1.start();
 		t2.start();
 		
-		//等待两个线程执行结束
 		t1.join();
 		t2.join();
 		
 		return test.count;
-		
 	}
 	
-	
-	public static void main(String[] args) throws InterruptedException  {
-		int c=calc();
-		System.out.println(c);
+	public static void main(String[] args) throws InterruptedException {
+		int result=calc();
+		System.out.println(result);
 	}
 
 }

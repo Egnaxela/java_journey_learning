d7836ccd0345e3a6766e60e5b3cf362b4cb58e4e
Egnaxela <yougensai@gmail.com>
2019-03-26 18:52:41 +0800

    :whale:

---
+++ src/com/designPattern/creational/factorymethod/Test.java
@@ -0,0 +1,30 @@
+/**  
+ * FileName:     
+ * @Description: 
+ * Company       
+ * @version      1.0
+ * @author:      Pluto
+ * @version:     1.0
+ * Createdate:   2019年3月26日 下午6:47:57  
+ *  
+ */  
+
+package com.designPattern.creational.factorymethod;
+
+/**  
+ * Description:   
+ * Copyright:   Copyright (c)2019 
+ * Company:       
+ * @author:     Pluto 
+ * @version:    1.0  
+ * Create at:   2019年3月26日 下午6:47:57  
+ *  
+ * Modification History:  
+ * Date         Author      Version     Description  
+ * ------------------------------------------------------------------  
+ * 2019年3月26日      Pluto       1.0         1.0 Version  
+ */
+
+public class Test {
+
+}

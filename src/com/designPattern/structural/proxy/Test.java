a0ee06e7446931a71fd64f12c54bafd31f6dff10
Egnaxela <yougensai@gmail.com>
2019-03-28 19:16:43 +0800

    proxy

---
+++ src/com/designPattern/structural/proxy/Test.java
@@ -0,0 +1,30 @@
+/**  
+ * FileName:     
+ * @Description: 
+ * Company       
+ * @version      1.0
+ * @author:      Pluto
+ * @version:     1.0
+ * Createdate:   2019年3月28日 下午7:13:05  
+ *  
+ */  
+
+package com.designPattern.structural.proxy;
+
+/**  
+ * Description:   
+ * Copyright:   Copyright (c)2019 
+ * Company:       
+ * @author:     Pluto 
+ * @version:    1.0  
+ * Create at:   2019年3月28日 下午7:13:05  
+ *  
+ * Modification History:  
+ * Date         Author      Version     Description  
+ * ------------------------------------------------------------------  
+ * 2019年3月28日      Pluto       1.0         1.0 Version  
+ */
+
+public class Test {
+
+}
